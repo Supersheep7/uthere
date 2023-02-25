bebd19c3bd635ab04d75f81c2687edda1342c19a
Supersheep7 <alessandro.corona.m@gmail.com>
2023-02-25 18:57:17 +0100

    Added Maps API, boilerplate header

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -28,42 +28,31 @@ export default function Layout({ children, home }: any) {
       <header className={styles.header}>
         {home ? (
           <>
-            <Image
-              priority
-              src="/images/profile.jpg"
-              className={utilStyles.borderCircle}
-              height={144}
-              width={144}
-              alt=""
-            />
-            <h1 className={utilStyles.heading2Xl}>{name}</h1>
+            <HeaderHome />
           </>
         ) : (
           <>
-            <Link href="/">
-              <Image
-                priority
-                src="/images/profile.jpg"
-                className={utilStyles.borderCircle}
-                height={108}
-                width={108}
-                alt=""
-              />
-            </Link>
-            <h2 className={utilStyles.headingLg}>
-              <Link href="/" className={utilStyles.colorInherit}>
-                {name}
-              </Link>
-            </h2>
+            <HeaderGeneric />
           </>
         )}
       </header>
-      <main>{children}</main>
-      {!home && (
-        <div className={styles.backToHome}>
-          <Link href="/">← Back to home</Link>
-        </div>
-      )}
+      {children}
     </div>
   );
+}
+
+function HeaderHome(): any {
+  return (
+    <div>
+      <h1>uThere?</h1>
+    </div>
+  )
+}
+
+function HeaderGeneric(): any {
+  return (
+    <div>
+      <h1>uThere?</h1>
+    </div>
+  )
 }
